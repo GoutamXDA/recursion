90c8b0259b0ea254ea50213c3eb1d6cf48f599c1
Goutam <goutambyte@gmail.com>
2024-01-26 23:32:37 +0530

    esay

--- src/easy/Nto1.java
+++ src/easy/Nto1.java
@@ -5,7 +5,9 @@ public class Nto1 {
 	public static void main(String[] args) {
 		int number=5;
 		numberPrint(number);
+		numberPrintReverse(number);
 	}
+	// decreasing order
 	public static void numberPrint(int number) {
 		if(number==0) {
 			return ;
@@ -13,4 +15,13 @@ public class Nto1 {
 		System.out.println(number);
 		numberPrint(number-1);
 	}
+	// increasing order
+	public static void numberPrintReverse(int number) {
+		if(number==0) {
+			return ;
+		}
+		numberPrintReverse(number-1);
+		System.out.println(number);
+	}
+	
 }
