e7b682e96e84e3a886a79793882e1451f794e60f
Goutam <goutambyte@gmail.com>
2024-01-27 17:31:03 +0530

    linear search

---
+++ src/array/Find.java
@@ -0,0 +1,24 @@
+package array;
+
+public class Find {
+
+	public static void main(String[] args) {
+		int[] nums= {1,23,12,29,0,11};
+		System.out.println(checkElement(nums,0,0));
+		
+		System.out.println(findIndex(nums,0,0));
+	}
+
+	private static int findIndex(int[] nums, int target, int index) {
+		if(index==nums.length) return -1;
+		
+		if(nums[index]==target) return index;
+		else return findIndex(nums, target, index+1);
+	}
+
+	private static boolean checkElement(int[] nums,int target,int index) {
+		if(index==nums.length) return false;
+		return nums[index]==target || checkElement(nums, target, index+1);
+	}
+}
+
