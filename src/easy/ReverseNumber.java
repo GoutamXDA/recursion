055d7ff5a65143361cc50fac4bf308f8b7a626df
Goutam <goutambyte@gmail.com>
2024-01-27 11:24:41 +0530

    reverse number

---
+++ src/easy/ReverseNumber.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class ReverseNumber {
+
+	static int sum=0;
+	public static void main(String[] args) {
+		int n=1234;
+		reverse(n);
+		System.out.println(sum);
+		
+	}
+
+	private static void reverse(int n) {
+		if(n==0) return;
+		int rem=n%10;
+		sum=sum*10+rem;
+		reverse(n/10);
+		
+	}
+}
